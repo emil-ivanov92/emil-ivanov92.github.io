9c4f3f147fc566ce17d9444229bdb3db00af32d2
emil-ivanov92 <107074220+emil-ivanov92@users.noreply.github.com>
2022-06-09 23:13:16 +0300

    09.06.2022 - password removed
    09.06.2022 - password removed

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,15 +1,14 @@
-﻿
-var password;
-var pass1 = "cool";
+﻿// var password;
+// var pass1 = "cool";
 
-password = prompt('Please enter your password to view this page!',' ');
+// password = prompt('Please enter your password to view this page!',' ');
 
-if (password == pass1){
-  alert('Password Correct! Click OK to enter!');
-}
-else{
-window.location = "http://www.google.bg";
-}
+// if (password == pass1){
+//   alert('Password Correct! Click OK to enter!');
+// }
+// else{
+//     window.location = "http://www.google.bg";
+// }
 
 
 $(document).ready(function(){
